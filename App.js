1dc8b99e3975bb6567cb6d5afda00ab2b866f2c4
Scott DeVito <scottadevito@gmail.com>
2017-09-11 21:06:11 -0400

    Add dayHeader to App

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={[styles.dayHeader]}>
+          <Text h4 style={styles.day}>
+            Monday
+          </Text>
+          <Text style={styles.date}>October 3rd</Text>
+        </View>
       </View>
     );
   }
 }
 
+const { width, height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
+    height: height,
+    width: width,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+  dayHeader: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 75,
+    position: 'absolute',
+    paddingTop: Constants.statusBarHeight,
+    left: 0,
+    right: 0,
+    top: 0,
+    backgroundColor: '#4F4F4F',
+    zIndex: 10,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  day: {
+    color: '#ffffff',
+  },
+  date: {
+    color: '#ffffff',
   },
 });
