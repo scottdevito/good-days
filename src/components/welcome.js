7abdb702372f6ba3b48538b14211b894204c6e9e
Scott DeVito <scottadevito@gmail.com>
2017-10-08 20:35:31 -0400

    Add MainNavigator to App, Add routes, Move dayData to DayScreen,

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,31 +1,53 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 
 export const Welcome = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.sky}>
-        <Text style={welcomeText}>What's a good day look like for you?</Text>
+        <Text style={styles.welcomeText}>
+          What's a good day look like for you?
+        </Text>
       </View>
-      <View style={styles.mountains} />
+      <View style={styles.mountains}>
+        <Image
+          source={require('../../assets/welcome-logo.png')}
+          fadeDuration={0}
+          resizeMode={'contain'}
+        />
+      </View>
       <View style={styles.grass} />
     </View>
   );
 };
 
+const { width, height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
+  container: {
+    height: height,
+    width: width,
+  },
   sky: {
     flex: 2,
-    backgroundColor: "white"
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00BCD4',
   },
   mountains: {
-    height: 1,
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width,
   },
   grass: {
     flex: 1,
-    color: "#ffffff"
+    backgroundColor: '#8BC34A',
   },
   welcomeText: {
-    fontSize: 22
-  }
+    fontSize: 28,
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
--- src/screens/day_screen.js
+++ src/screens/day_screen.js
@@ -1,12 +1,78 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, Dimensions, ScrollView } from "react-native";
-import { Constants } from "expo";
-import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
-import { CheckBox } from 'react-native-elements'
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { Constants } from 'expo';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { CheckBox } from 'react-native-elements';
 
-import { DayFragment } from "../components/day_fragment";
-import { DayHeader } from "../components/day_header";
-import { Items } from "../components/items";
+import { DayFragment } from '../components/day_fragment';
+import { DayHeader } from '../components/day_header';
+import { Items } from '../components/items';
+
+const dayData = {
+  morningData: {
+    name: 'Morning',
+    fragmentColor: '#00bcd4',
+    weather: 'rain',
+    items: [
+      {
+        id: '09829',
+        title: 'Cold shower',
+        description: 'Take a cold shower.',
+        complete: false,
+      },
+      {
+        id: '3920',
+        title: 'Read a book',
+        description: 'Read an interesting book.',
+        complete: true,
+      },
+    ],
+  },
+  afternoonData: {
+    name: 'Afternoon',
+    fragmentColor: '#ffc107',
+    weather: 'rain',
+    items: [
+      {
+        id: '0945829',
+        title: 'Yoga class',
+        description: 'Emerge vinyassa class.',
+        complete: false,
+      },
+      {
+        id: '0982913',
+        title: 'Meditate',
+        description: 'Meditate for 15 minutes.',
+        complete: true,
+      },
+      {
+        id: '0239829',
+        title: 'Work at coffee shop',
+        description: 'Do some programming at a coffee shop.',
+        complete: false,
+      },
+    ],
+  },
+  eveningData: {
+    name: 'Evening',
+    fragmentColor: '#ff5722',
+    weather: 'rain',
+    items: [
+      {
+        id: '09252829',
+        title: 'Cold shower',
+        description: 'Take a cold shower.',
+        complete: false,
+      },
+      {
+        id: '09811229',
+        title: 'Read a book',
+        description: 'Read an interesting book.',
+        complete: true,
+      },
+    ],
+  },
+};
 
 export default class DayScreen extends Component {
   _renderFragment({ fragmentColor, name }) {
@@ -14,12 +80,12 @@ export default class DayScreen extends Component {
       <View
         style={{
           flex: 1,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-around",
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-around',
           width: width,
           height: 80,
-          backgroundColor: fragmentColor
+          backgroundColor: fragmentColor,
         }}
       >
         <MaterialIcons name="wb-sunny" size={28} color="white" />
@@ -34,58 +100,51 @@ export default class DayScreen extends Component {
   _renderItems({ items }) {
     return items.map(item => {
       return (
-        <View key={item.id} >
-        <CheckBox
-          title={item.title}
-          checked={true}
-          iconType='material'
-          checkedIcon='check'
-          uncheckedIcon='check-box-outline-blank'
-          checkedColor='black'
-          checked={false}
-        />
+        <View key={item.id}>
+          <CheckBox
+            title={item.title}
+            checked={true}
+            iconType="material"
+            checkedIcon="check"
+            uncheckedIcon="check-box-outline-blank"
+            checkedColor="black"
+            checked={false}
+          />
         </View>
       );
-    })
+    });
   }
 
   render() {
-    console.log(this.props.dayData.morningData);
     return (
-        <View style={{flex: 1}}>
-          <DayHeader />
-          <ScrollView style={styles.fragmentsContainer}>
-            <DayFragment
-              _renderFragment={this._renderFragment}
-              data={this.props.dayData.morningData}
-            />
-              <Items 
-                _renderItems={this._renderItems} 
-                data={this.props.dayData.morningData} 
-              />
-            <DayFragment
-              _renderFragment={this._renderFragment}
-              data={this.props.dayData.afternoonData}
-            />
-              <Items 
-                _renderItems={this._renderItems} 
-                data={this.props.dayData.afternoonData} 
-              />
-            <DayFragment
-              _renderFragment={this._renderFragment}
-              data={this.props.dayData.eveningData}
-            />
-              <Items 
-                _renderItems={this._renderItems} 
-                data={this.props.dayData.eveningData} 
-              />
-          </ScrollView>
+      <View style={{ flex: 1 }}>
+        <DayHeader />
+        <ScrollView style={styles.fragmentsContainer}>
+          <DayFragment
+            _renderFragment={this._renderFragment}
+            data={dayData.morningData}
+          />
+          <Items _renderItems={this._renderItems} data={dayData.morningData} />
+          <DayFragment
+            _renderFragment={this._renderFragment}
+            data={dayData.afternoonData}
+          />
+          <Items
+            _renderItems={this._renderItems}
+            data={dayData.afternoonData}
+          />
+          <DayFragment
+            _renderFragment={this._renderFragment}
+            data={dayData.eveningData}
+          />
+          <Items _renderItems={this._renderItems} data={dayData.eveningData} />
+        </ScrollView>
       </View>
     );
   }
 }
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   fragmentsContainer: {
@@ -93,14 +152,14 @@ const styles = StyleSheet.create({
     width: width,
   },
   fragmentText: {
-    color: "#ffffff",
-    fontWeight: "bold",
+    color: '#ffffff',
+    fontWeight: 'bold',
     fontSize: 18,
   },
   fragmentTextWrapper: {
-    width: "50%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    width: '50%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
