8cc607c1a7ae5a61a5604e0e2952bd023b72ce40
Scott DeVito <scottadevito@gmail.com>
2017-10-08 20:36:05 -0400

    Add new welcome screen image

--- src/screens/welcome_screen.js
+++ src/screens/welcome_screen.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { View } from "react-native";
-import { Welcome } from './componenets/welcome';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Welcome } from '../components/welcome';
 
 export const WelcomeScreen = () => {
   return (
     <View>
-        <Welcome />
+      <Welcome />
     </View>
   );
 };
