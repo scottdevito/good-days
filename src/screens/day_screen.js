eaede4a16b44c6e2d1c0e938b737378bd597ff17
Scott DeVito <scottadevito@gmail.com>
2017-10-09 22:37:51 -0400

    Implement new render setup and wire up touchable collapsing of fragments

--- src/screens/day_screen.js
+++ src/screens/day_screen.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  TouchableHighlight,
+  Easing,
+} from 'react-native';
 import { Constants } from 'expo';
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { CheckBox } from 'react-native-elements';
+import Collapsible from 'react-native-collapsible';
 
 import { DayFragment } from '../components/day_fragment';
 import { DayHeader } from '../components/day_header';
@@ -75,27 +84,48 @@ const dayData = {
 };
 
 export default class DayScreen extends Component {
+  state = {
+    morningCollapsed: true,
+    afternoonCollapsed: true,
+    eveningCollapsed: true,
+  };
+
   _renderFragment({ fragmentColor, name }) {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-          width: width,
-          height: 80,
-          backgroundColor: fragmentColor,
+      <TouchableHighlight
+        onPress={() => {
+          this._toggleExpanded(name);
         }}
       >
-        <MaterialIcons name="wb-sunny" size={28} color="white" />
-        <View style={styles.fragmentTextWrapper}>
-          <Text style={styles.fragmentText}>{name}</Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            width: width,
+            height: 80,
+            backgroundColor: fragmentColor,
+          }}
+        >
+          <MaterialIcons name="wb-sunny" size={28} color="white" />
+          <View style={styles.fragmentTextWrapper}>
+            <Text style={styles.fragmentText}>{name}</Text>
+          </View>
+          <MaterialCommunityIcons name="plus" size={34} color="white" />
         </View>
-        <MaterialCommunityIcons name="plus" size={34} color="white" />
-      </View>
+      </TouchableHighlight>
     );
   }
+
+  // Open and close fragments based on which fragment is pressed
+  _toggleExpanded = name => {
+    let fragmentName = `${name.toLowerCase()}Collapsed`;
+
+    this.setState((prevState, props) => {
+      return { [fragmentName]: !prevState[fragmentName] };
+    });
+  };
 
   _renderItems({ items }) {
     return items.map(item => {
@@ -120,7 +150,37 @@ export default class DayScreen extends Component {
       <View style={{ flex: 1 }}>
         <DayHeader />
         <ScrollView style={styles.fragmentsContainer}>
-          <DayFragment
+          {this._renderFragment(dayData.morningData)}
+          <Collapsible
+            duration={700}
+            collapsed={this.state.morningCollapsed}
+            align="center"
+            easing={Easing.inOut(Easing.quad)}
+          >
+            <View>{this._renderItems(dayData.morningData)}</View>
+          </Collapsible>
+
+          {this._renderFragment(dayData.afternoonData)}
+          <Collapsible
+            duration={700}
+            collapsed={this.state.afternoonCollapsed}
+            align="center"
+            easing={Easing.inOut(Easing.quad)}
+          >
+            <View>{this._renderItems(dayData.afternoonData)}</View>
+          </Collapsible>
+
+          {this._renderFragment(dayData.eveningData)}
+          <Collapsible
+            duration={700}
+            collapsed={this.state.eveningCollapsed}
+            align="center"
+            easing={Easing.inOut(Easing.quad)}
+          >
+            <View>{this._renderItems(dayData.eveningData)}</View>
+          </Collapsible>
+
+          {/* <DayFragment
             _renderFragment={this._renderFragment}
             data={dayData.morningData}
           />
@@ -137,7 +197,7 @@ export default class DayScreen extends Component {
             _renderFragment={this._renderFragment}
             data={dayData.eveningData}
           />
-          <Items _renderItems={this._renderItems} data={dayData.eveningData} />
+          <Items _renderItems={this._renderItems} data={dayData.eveningData} /> */}
         </ScrollView>
       </View>
     );
