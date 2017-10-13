b70d76e90289aa357485360edd548bc11ae53d3a
Scott DeVito <scottadevito@gmail.com>
2017-10-13 14:31:26 -0400

    Change from Tab Navigator to Stack Navigator with button to change route

--- App.js
+++ App.js
@@ -7,32 +7,28 @@ import {
   ScrollView,
   StatusBar,
 } from 'react-native';
-import {
-  TabNavigator,
-  StackNavigator,
-  DrawerNavigator,
-} from 'react-navigation';
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
 
 import DayScreen from './src/screens/day_screen';
 import { WelcomeScreen } from './src/screens/welcome_screen';
 
 export default class App extends Component {
   render() {
-    const MainNavigator = TabNavigator({
+    const MainNavigator = StackNavigator({
       welcome: {
         screen: WelcomeScreen,
         navigationOptions: ({ navigation }) => ({
-          tabBarVisible: false,
+          header: null,
         }),
       },
       mainFlow: {
         screen: DrawerNavigator({
           dayScreen: {
             screen: DayScreen,
-            navigationOptions: ({ navigation }) => ({
-              tabBarVisible: false,
-            }),
           },
+        }),
+        navigationOptions: ({ navigation }) => ({
+          header: null,
         }),
       },
     });
