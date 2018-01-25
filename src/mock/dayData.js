6aa0fbd88634588e2cc9048e99bd5001324389e5
Scott DeVito <scottadevito@gmail.com>
2018-01-24 21:19:28 -0500

    Move mock data out of DayScreen and into its own file

---
+++ src/mock/dayData.js
@@ -0,0 +1,88 @@
+export const dayData = {
+  morningData: {
+    name: 'Morning',
+    id: 'morning',
+    fragmentColor: '#4dd0e1',
+    weather: 'rain',
+    items: [
+      {
+        id: '09829',
+        title: 'Cold shower',
+        description: 'Take a cold shower.',
+        complete: false,
+      },
+      {
+        id: '3920',
+        title: 'Read a book',
+        description: 'Read an interesting book.',
+        complete: true,
+      },
+    ],
+  },
+  afternoonData: {
+    name: 'Afternoon',
+    id: 'afternoon',
+    fragmentColor: '#ffd54f',
+    weather: 'rain',
+    items: [
+      {
+        id: '0945829',
+        title: 'Yoga class',
+        description: 'Emerge vinyassa class.',
+        complete: true,
+      },
+      {
+        id: '0982913',
+        title: 'Meditate',
+        description: 'Meditate for 15 minutes.',
+        complete: true,
+      },
+      {
+        id: '0239829',
+        title: 'Work at coffee shop',
+        description: 'Do some programming at a coffee shop.',
+        complete: false,
+      },
+      {
+        id: '0233429',
+        title: 'Go for a run',
+        description: 'Go for a run to the beach',
+        complete: false,
+      },
+    ],
+  },
+  eveningData: {
+    name: 'Evening',
+    id: 'evening',
+    fragmentColor: '#ff8a65',
+    weather: 'rain',
+    items: [
+      {
+        id: '09252829',
+        title: 'Cold shower',
+        description: 'Take a cold shower.',
+        complete: true,
+      },
+      {
+        id: '09811229',
+        title: 'Read a book',
+        description: 'Read an interesting book.',
+        complete: true,
+      },
+    ],
+  },
+  allDayData: {
+    name: 'All Day',
+    id: 'allDay',
+    fragmentColor: '#81c784',
+    weather: 'rain',
+    items: [
+      {
+        id: '09252829',
+        title: 'Breathing exercises while driving',
+        description: 'Take a cold shower.',
+        complete: false,
+      },
+    ],
+  },
+};
--- src/screens/day_screen.js
+++ src/screens/day_screen.js
@@ -13,91 +13,9 @@ import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { CheckBox } from 'react-native-elements';
 import Collapsible from 'react-native-collapsible';
 
+import { dayData } from '../mock/dayData';
 import { DayHeader } from '../components/day_header';
 import { StatusBar } from '../components/status_bar';
-
-const dayData = {
-  morningData: {
-    name: 'Morning',
-    id: 'morning',
-    fragmentColor: '#4dd0e1',
-    weather: 'rain',
-    items: [
-      {
-        id: '09829',
-        title: 'Cold shower',
-        description: 'Take a cold shower.',
-        complete: false,
-      },
-      {
-        id: '3920',
-        title: 'Read a book',
-        description: 'Read an interesting book.',
-        complete: true,
-      },
-    ],
-  },
-  afternoonData: {
-    name: 'Afternoon',
-    id: 'afternoon',
-    fragmentColor: '#ffd54f',
-    weather: 'rain',
-    items: [
-      {
-        id: '0945829',
-        title: 'Yoga class',
-        description: 'Emerge vinyassa class.',
-        complete: false,
-      },
-      {
-        id: '0982913',
-        title: 'Meditate',
-        description: 'Meditate for 15 minutes.',
-        complete: true,
-      },
-      {
-        id: '0239829',
-        title: 'Work at coffee shop',
-        description: 'Do some programming at a coffee shop.',
-        complete: false,
-      },
-    ],
-  },
-  eveningData: {
-    name: 'Evening',
-    id: 'evening',
-    fragmentColor: '#ff8a65',
-    weather: 'rain',
-    items: [
-      {
-        id: '09252829',
-        title: 'Cold shower',
-        description: 'Take a cold shower.',
-        complete: true,
-      },
-      {
-        id: '09811229',
-        title: 'Read a book',
-        description: 'Read an interesting book.',
-        complete: true,
-      },
-    ],
-  },
-  allDayData: {
-    name: 'All Day',
-    id: 'allDay',
-    fragmentColor: '#81c784',
-    weather: 'rain',
-    items: [
-      {
-        id: '09252829',
-        title: 'Breathing exercises while driving',
-        description: 'Take a cold shower.',
-        complete: false,
-      },
-    ],
-  },
-};
 
 export default class DayScreen extends Component {
   static navigationOptions = {
